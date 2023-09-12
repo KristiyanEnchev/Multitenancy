bbf05614d65b7b8ca88d277684450869c634a8fd
Kiko <kristiqnenchevv@gmail.com>
2023-09-12 21:41:32 +0300

    Add new branch

--- Multitenant.API/Multitenant.Domain/Entities/DomainEvent.cs
+++ Multitenant.API/Multitenant.Domain/Entities/DomainEvent.cs
@@ -1,7 +1,6 @@
 namespace Multitenant.Domain.Entities
 {
     using Multitenant.Shared.Events;
-
     public abstract class DomainEvent : IEvent
     {
         public DateTime TriggeredOn { get; protected set; } = DateTime.UtcNow;
